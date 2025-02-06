d20a92f095cb32c008dc88f749ad7540246cc41b
Diana Ortiz <diana.lopezofi16@gmai.com>
2025-02-05 22:20:15 -0600

    Se ha agregado una imagen principal en tipos de datos

---
+++ tiposDeDatos.js
@@ -0,0 +1,33 @@
+//Tipos de Datos
+//number
+console.log(typeof 42)
+
+//String
+console.log(typeof 'Veinticinco')
+
+//number
+console.log(typeof -666)
+
+//boolean
+console.log(typeof true)
+
+//number
+console.log(typeof 0)
+
+//string
+console.log(typeof "")
+
+//Object
+console.log(typeof null)
+
+//undefined
+console.log(typeof undefined)
+
+//boolean
+console.log(typeof false)
+
+//number
+console.log(typeof 789)
+
+//boolean
+console.log( typeof true)
