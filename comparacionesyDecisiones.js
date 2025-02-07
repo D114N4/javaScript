34ec580fc5beeeb91992ca6d6e78652703e272a5
Diana Ortiz <diana.lopezofi16@gmai.com>
2025-02-06 21:24:30 -0600

    Se creo el trabajo pedido en clase con un alumno

---
+++ comparacionesyDecisiones.js
@@ -0,0 +1,16 @@
+//Evaluacion de nota estudiante
+
+let estudiante = 75;
+
+if (estudiante >= 90) {
+    console.log("Excelente has aprobado con honores")
+}
+else if (estudiante <= 89 && estudiante >= 75) {
+    console.log(" Bien has aprobado")
+}
+else if (estudiante <= 74 && estudiante >= 60) {
+    console.log("Suficiente has obtenido una calificacion aprobatoria, deberías hecharle mas ganas")
+}
+else {
+    console.log("Reprobaste")
+}
